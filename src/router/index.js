1618c8f282db115f24aaac4d2c1ed384e6ea5daf
emily3231 <emily3231@hotmail.com>
2025-10-19 12:27:56 +0800

    update all files

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../views/Login.vue'
 import Home from '../views/Home.vue'
-import New from '../views/New.vue' 
-import Send from '../views/Send.vue' 
-import Share from '../views/Share.vue' 
+import New from '../views/New.vue'
+import Send from '../views/Send.vue'
+import Share from '../views/Share.vue'
 
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', name: 'Login', component: () => import('../views/Login.vue') },
-  { path: '/home', name: 'Home', component: () => import('../views/Home.vue')},
-  { path: '/new', name: 'New', component: () => import('../views/New.vue') }, 
-  { path: '/send', name: 'Send',  component: () => import('../views/Send.vue') }, 
-  { path: '/share', name: 'Share', component: () => import('../views/Share.vue')}, // 🆕 新頁面
+  { path: '/home', name: 'Home', component: () => import('../views/Home.vue') },
+  { path: '/new', name: 'New', component: () => import('../views/New.vue') },
+  { path: '/send', name: 'Send', component: () => import('../views/Send.vue') },
+  { path: '/share', name: 'Share', component: () => import('../views/Share.vue') },
 ]
 
 const router = createRouter({
-  history: createWebHistory('/white4/'), // 保持原本的 base
+  history: createWebHistory(), // ✅ 移除 '/white5/'
   routes,
 })
 
